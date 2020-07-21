37ef91dddff7aad711d01e099bad490c02338966
JeremyGlik <glik.jeremy@gmail.com>
2020-07-21 21:44:06 +0200

    premier commit

---
+++ src/js/burger.js
@@ -0,0 +1,9 @@
+var burger  = document.querySelector(".burger-menu");
+var menu  = document.querySelector(".menu-mobile");
+
+burger.addEventListener("click", function() { 
+    menu.classList.toggle("show-menu");
+
+
+
+} )
